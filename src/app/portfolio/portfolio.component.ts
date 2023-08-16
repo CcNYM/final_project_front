1eefc2c13a6a6da40ad36a8fa2dea415c99da02f
Tianqi <1144069701@qq.com>
2023-08-16 05:14:17 +0000

    add timer in portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
@@ -10,32 +10,41 @@ import { UserApiService } from '../api-service/user-api.service';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit{
+export class PortfolioComponent implements OnInit, OnDestroy {
 
   principle: number = 0;
   stocks: Portfolio[] = [];
   public shouldShowSellButton: boolean = false;
-  
+
+  timer: any;
 
   constructor(
     private router: Router,
     private userService: UserApiService,
     private dialog: MatDialog
-    ) {}
+  ) { }
 
-  
-  async ngOnInit(){
-      await this.loadPrinciple(1);
-      await this.loadPortfolio(1);
-      if (this.stocks.length != 0){
-        this.shouldShowSellButton = true
-      }
 
-      console.log(this.shouldShowSellButton)
+  async ngOnInit() {
+    await this.loadPrinciple(1);
+    await this.loadPortfolio(1);
+    if (this.stocks.length != 0) {
+      this.shouldShowSellButton = true
+    }
+    this.timer = setInterval(() => {
+      this.loadPrinciple(1);
+      this.loadPortfolio(1);
+      console.log("+10s")
+    }, 10000)
+    //console.log(this.shouldShowSellButton)
   }
 
-  async loadPrinciple(userId: number){
-      return new Promise((resolve, reject) =>{
+  ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
+
+  async loadPrinciple(userId: number) {
+    return new Promise((resolve, reject) => {
       this.userService.getUserPrincipleHoldings(userId).subscribe({
         next: async (principle: number) => {
           this.principle = principle
@@ -47,22 +56,22 @@ export class PortfolioComponent implements OnInit{
         }
       })
     })
-  } 
+  }
 
-  async loadPortfolio(userId: number){
-    return new Promise((resolve, reject) =>{
-    this.userService.getPortfolioList(userId).subscribe({
-      next: async (stocks: Portfolio[]) => {
-        this.stocks = stocks
-        resolve(reject)
-      },
-      error: async (error) => {
-        console.error('Error occured' + error)
-        reject()
-      }
+  async loadPortfolio(userId: number) {
+    return new Promise((resolve, reject) => {
+      this.userService.getPortfolioList(userId).subscribe({
+        next: async (stocks: Portfolio[]) => {
+          this.stocks = stocks
+          resolve(reject)
+        },
+        error: async (error) => {
+          console.error('Error occured' + error)
+          reject()
+        }
+      })
     })
-  })
-} 
+  }
 
   // async loadPortfolio(userId: number){
   //   this.userService.getPortfolioList(userId).subscribe({
@@ -88,9 +97,9 @@ export class PortfolioComponent implements OnInit{
     // dialogConfig.panelClass = 'confirmation-dialog-container';
     dialogConfig.data = { message: 'Do you confirm the sale?' };
     dialogConfig.position = { top: '20%' };
-  
+
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, dialogConfig);
-  
+
     dialogRef.afterClosed().subscribe(result => {
       if (result === true) {
         // 用户确认销售
